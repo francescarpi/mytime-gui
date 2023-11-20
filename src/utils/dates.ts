28f57cc06b0d7b33f6c8b41b153fdf6afa3d15ad
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-11-20 08:14:39 +0100

    feat(table): allow anvigate through dates using keyboard

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,3 +1,5 @@
+const locale = "en-GB";
+
 /**
  * Given a duration represented in seconds and returns a string with hours and minutes.
  * Example: 7552 => 2h6m
@@ -11,32 +13,35 @@ export const formatDuration = (duration: number): string => {
 /**
  * Given a date, returns a string with format: HH:mm
  */
-export const dateToStrTime = (date: Date): string => {
-  return new Intl.DateTimeFormat("es-ES", {
+export const dateToStrTime = (date: Date): string =>
+  new Intl.DateTimeFormat("es-ES", {
     timeZone: "Europe/Madrid",
     hour: "2-digit",
     minute: "2-digit",
   }).format(date);
-};
 
 /**
  * Given a date, returns a string with format YYYY-MM-DD
  */
-export const dateToStrDate = (date: Date): string => {
-  return new Intl.DateTimeFormat("fr-CA", {
+export const dateToStrDate = (date: Date): string =>
+  new Intl.DateTimeFormat("fr-CA", {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
   }).format(date);
-};
 
 /**
  * Given a date, returns a string with format YYYY-MM-DD HH:mm
  */
-export const dateToStrDateTime = (date: Date): string => {
-  return new Intl.DateTimeFormat("es-ES", {
+export const dateToStrDateTime = (date: Date): string =>
+  new Intl.DateTimeFormat("es-ES", {
     dateStyle: "long",
     timeStyle: "short",
     timeZone: "Europe/Madrid",
   }).format(date);
-};
+
+export const dayOfTheWeek = (date: Date): string =>
+  new Intl.DateTimeFormat(locale, {
+    weekday: "long",
+    timeZone: "Europe/Madrid",
+  }).format(date);
