c9adb4f91679ad4c1392cd79db5d936133834702
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-11-27 15:07:07 +0100

    feat(footer): fixed summary reactivity

--- src/components/SummaryGoal.vue
+++ src/components/SummaryGoal.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import { formatDuration } from "@/utils/dates";
 
 const { title, value, goal } = defineProps<{
@@ -8,20 +7,12 @@ const { title, value, goal } = defineProps<{
   goal?: number;
 }>();
 
-const className = computed(() => {
-  if (!goal) {
-    return "";
-  }
-
-  return value - goal < 0 ? "text-red-7" : "text-green-8";
-});
-
 </script>
 
 <template>
   <div>
     {{ title }}:
-    <span class="text-bold q-pl-xs" :class="className">
+    <span class="text-bold q-pl-xs" :class="!goal ? '' : value - goal < 0 ? 'text-red-7' : 'text-green-8'">
       {{ formatDuration(value) }}
     </span>
   </div>
