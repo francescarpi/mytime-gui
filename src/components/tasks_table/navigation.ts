909e6db0704ece215b0b639a0b509612a177cf68
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-11-29 14:33:10 +0100

    fix(keybinding): fix press number in inputs

--- src/components/tasks_table/navigation.ts
+++ src/components/tasks_table/navigation.ts
@@ -6,43 +6,51 @@ export function useNavigation() {
   const { openTaskNumber } = useTaskActions()
 
   const listenKeyDown = (e: KeyboardEvent) => {
-    switch (e.code) {
-      case "ArrowLeft":
-        previousFilterDate()
-        break
-      case "ArrowRight":
-        nextFilterDate()
-        break
-      case "ArrowDown":
-        todayFilterDate()
-        break
-      case "Digit1":
-        openTaskNumber(1)
-        break
-      case "Digit2":
-        openTaskNumber(2)
-        break
-      case "Digit3":
-        openTaskNumber(3)
-        break
-      case "Digit4":
-        openTaskNumber(4)
-        break
-      case "Digit5":
-        openTaskNumber(5)
-        break
-      case "Digit6":
-        openTaskNumber(6)
-        break
-      case "Digit7":
-        openTaskNumber(7)
-        break
-      case "Digit8":
-        openTaskNumber(8)
-        break
-      case "Digit9":
-        openTaskNumber(9)
-        break
+    if ((e.target as HTMLInputElement).tagName.toLowerCase() === "input") {
+      return
+    }
+    if (e.altKey) {
+      switch (e.code) {
+        case "Digit1":
+          openTaskNumber(1)
+          break
+        case "Digit2":
+          openTaskNumber(2)
+          break
+        case "Digit3":
+          openTaskNumber(3)
+          break
+        case "Digit4":
+          openTaskNumber(4)
+          break
+        case "Digit5":
+          openTaskNumber(5)
+          break
+        case "Digit6":
+          openTaskNumber(6)
+          break
+        case "Digit7":
+          openTaskNumber(7)
+          break
+        case "Digit8":
+          openTaskNumber(8)
+          break
+        case "Digit9":
+          openTaskNumber(9)
+          break
+      }
+    } else {
+      switch (e.code) {
+        case "ArrowLeft":
+          previousFilterDate()
+          break
+        case "ArrowRight":
+          nextFilterDate()
+          break
+        case "ArrowDown":
+          todayFilterDate()
+          break
+      }
     }
   }
 
