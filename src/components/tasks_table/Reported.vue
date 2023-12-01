4d01d0e496bf37d7ea484322ebfc389a9282cf9a
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-12-01 18:21:42 +0100

    feat(table): improve reported icon color

--- src/components/tasks_table/Reported.vue
+++ src/components/tasks_table/Reported.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import { useSettingsStore } from "@/stores/settings"
+import { storeToRefs } from "pinia"
+
+const { settings } = storeToRefs(useSettingsStore())
 const { task } = defineProps(["task"])
 const { reported } = task
+const color = (reported: boolean, darkMode: boolean) => {
+  if (darkMode) {
+    return reported ? "text-white" : "text-grey-9"
+  }
+  return reported ? "text-black" : "text-grey-5"
+}
 </script>
 
 <template>
-  <q-icon :name="reported ? 'cloud_done' : 'cloud'" :class="reported ? 'text-black' : 'text-grey-5'"></q-icon>
+  <q-icon :name="reported ? 'cloud_done' : 'cloud'" :class="color(reported, settings.dark_mode)"></q-icon>
 </template>
