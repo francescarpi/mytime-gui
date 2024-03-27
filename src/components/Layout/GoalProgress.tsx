e35333d6374ce59925ddd8b39b1bcd3328103787
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2024-03-27 10:15:28 +0100

    feat(ui): add extra worked in the progress

--- src/components/Layout/GoalProgress.tsx
+++ src/components/Layout/GoalProgress.tsx
@@ -16,6 +16,9 @@ const GoalProgress = ({
   const percentage = goal === 0 ? 0 : (value * 100) / goal;
   const color = percentage < 100 ? "error" : "success";
 
+  const extra = goal - value;
+  const extraReal = extra >= 0 ? 0 : Math.abs(extra);
+
   return (
     <Box sx={{ ...sx, position: "relative" }}>
       <LinearProgress
@@ -31,13 +34,14 @@ const GoalProgress = ({
         sx={{
           position: "absolute",
           top: 0,
-          left: 10,
+          left: 0,
           textAlign: "center",
           width: "100%",
         }}
       >
         <Typography variant="subtitle2">
           {formatDuration(value)} of {formatDuration(goal)}
+          {extraReal > 0 && ` (+${formatDuration(extraReal)})`}
         </Typography>
       </Box>
     </Box>
