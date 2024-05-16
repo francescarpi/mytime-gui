2b37178a334757aafcdca823835278577bf7f853
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-05-16 05:13:16 +0200

    feat: some task table columns moved to a component

---
+++ src/components/TasksTable/ProjDescExtId.tsx
@@ -0,0 +1,71 @@
+import TableCell from "@mui/material/TableCell";
+import Box from "@mui/material/Box";
+import CopyStringToAddForm from "./CopyStringToAddForm";
+import CopyStringToClipboard from "./CopyStringToClipboard";
+import { Task } from "../../hooks/useTasks";
+
+const ProjDescExtId = ({
+  task,
+  dispatchDefaultAddTaskValues,
+  copyStringToClipboard,
+}: {
+  task: Task;
+  dispatchDefaultAddTaskValues: CallableFunction;
+  copyStringToClipboard: CallableFunction;
+}) => {
+  return (
+    <>
+      <TableCell>
+        <Box
+          sx={{
+            textWrap: "nowrap",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <CopyStringToAddForm
+            dispatchDefaultAddTaskValues={dispatchDefaultAddTaskValues}
+            type="setProj"
+            value={task.project}
+          />
+          <CopyStringToClipboard onClick={copyStringToClipboard}>
+            {task.project}
+          </CopyStringToClipboard>
+        </Box>
+      </TableCell>
+      <TableCell>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <CopyStringToAddForm
+            dispatchDefaultAddTaskValues={dispatchDefaultAddTaskValues}
+            type="setDesc"
+            value={task.desc}
+          />
+          <CopyStringToClipboard onClick={copyStringToClipboard}>
+            {task.desc}
+          </CopyStringToClipboard>
+        </Box>
+      </TableCell>
+      <TableCell align="right">
+        <Box
+          sx={{
+            textWrap: "nowrap",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+          }}
+        >
+          <CopyStringToAddForm
+            dispatchDefaultAddTaskValues={dispatchDefaultAddTaskValues}
+            type="setExtId"
+            value={task.external_id}
+          />
+          <CopyStringToClipboard onClick={copyStringToClipboard}>
+            {task.external_id}
+          </CopyStringToClipboard>
+        </Box>
+      </TableCell>
+    </>
+  );
+};
+
+export default ProjDescExtId;
