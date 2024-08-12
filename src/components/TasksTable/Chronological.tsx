3de2bf722507429abde4917fe4a73f65f12a4ecd
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-08-12 19:25:58 +0200

    refactor: change text

--- src/components/TasksTable/Chronological.tsx
+++ src/components/TasksTable/Chronological.tsx
@@ -44,9 +44,7 @@ const Chronological = ({
 }) => {
   const confirm = useConfirm();
   const deleteHandler = (id: number) => {
-    confirm({ description: "Are you sure you want to delete this task?" }).then(
-      () => deleteTask(id),
-    );
+    confirm({ description: "Delete task?" }).then(() => deleteTask(id));
   };
 
   return (
